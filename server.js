42977c09f7ef4a3aae2ab9e34c6868c41a4f6ace
DandanXO <pigpig850130@gmail.com>
2018-07-04 21:20:36 +0800

    update

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const app = require('./main');
-
-const port = process.env.port || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port);
